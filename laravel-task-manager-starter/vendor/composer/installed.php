df5e12629f190ac00793d216c16d5997623c3882
vinodi saumya <114313362+vinodi00@users.noreply.github.com>
2025-09-24 23:45:45 +0530

    Update installed.php

--- laravel-task-manager-starter/vendor/composer/installed.php
+++ laravel-task-manager-starter/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'af430da44d0bb2771900b7e815c56286a0fe7424',
+        'reference' => '4e1392ae979b61ee15cd6ed111f956950888a0a7',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'af430da44d0bb2771900b7e815c56286a0fe7424',
+            'reference' => '4e1392ae979b61ee15cd6ed111f956950888a0a7',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
